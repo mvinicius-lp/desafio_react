37299c9fb93c8ee96504f8905028d580a07e0656
Marcos Vinicius <mvinnisouslp87@gmail.com>
2022-09-21 01:39:36 -0300

    atualização

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 // components
 import Header from './components/Header';
 import Footer from './components/Footer';
+import TeskForm from './components/TeskForm';
+import TeskList from './components/TeskList';
+
+//CSS
+import styles from "./App.module.css";
+
 
 function App() {
   return (
     <div>
      <Header />
-     <h1>conteudo...</h1>
+     <main className={styles.main}>
+        <div>
+          <h2>Nota</h2>
+          <TeskForm btnText="Criar nota"/>
+        </div>
+        <div>
+          <h2>Suas notas</h2>
+          <TeskList/>
+        </div>
+     </main>
      <Footer />
     </div>
   );
---
+++ todo/src/components/TeskList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const TeskList = (props: Props) => {
+  return (
+    <div>TeskList</div>
+  )
+}
+
+export default TeskList
