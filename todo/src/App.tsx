8b9e072e9e74a08c4bb639536163f2ed8b8dee59
Marcos Vinicius <mvinnisouslp87@gmail.com>
2022-09-20 23:20:20 -0300

    primeiro commit

---
+++ todo/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+     <Header />
+     <h1>conteudo...</h1>
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ todo/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+    <footer>
+      <p>
+        <span>todo-app</span> @ 2022
+      </p>
+    </footer>
+  </footer>
+  )
+}
+
+export default Footer
